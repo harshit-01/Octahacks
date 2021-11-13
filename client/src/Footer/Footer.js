57e08bd6de31aab7825cdce12c458aeb91f9cc6d
Puneet Goel <puneetgoel016@gmail.com>
2021-11-13 22:56:38 +0530

    Footer css changed

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -5,27 +5,27 @@ function Footer(props){
 	return(
     <div className="footer mt-5 bg-primary">
         <div className="container">
-            <div className="row justify-content-center p-2 text-white">             
-                <div className="col-7 col-sm-5">
-                    <h5>Our Address</h5>
+            <div className="row justify-content-center text-white">             
+                <div className="col-7 col-sm-5 mt-3">
+                    <h5 className="font-weight-bold">Contact Us</h5>
                     <address>
 		              121, Clear Water Bay Road<br />
 		              Clear Water Bay, Kowloon<br />
 		              HONG KONG<br />
 		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
 		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net" >
-                         confusion@food.net</a>
+		              <i className="fa fa-envelope fa-lg"></i>: <Link to="mailto:confusion@food.net" className="text-white">
+                         confusion@food.net</Link>
                     </address>
                 </div>
-                <div className="col-12 col-sm-4 align-self-center">
+                <div className="col-12 col-sm-6 align-self-center">
                     <div className="text-center">
-                        <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
-                        <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-                        <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
+                        <a className="btn btn-social-icon btn-google bg-white  m-1" href="http://google.com/+"><i className="fa fa-google-plus fa-2x text-danger p-1"></i></a>
+                        <a className="btn btn-social-icon btn-facebook bg-white  m-1" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook fa-2x text-primary p-1"></i></a>
+                        <a className="btn btn-social-icon btn-linkedin bg-white  m-1" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin fa-2x text-info p-1"></i></a>
+                        <a className="btn btn-social-icon btn-twitter bg-white  m-1" href="http://twitter.com/"><i className="fa fa-twitter fa-2x text-primary p-1"></i></a>
+                        <a className="btn btn-social-icon btn-google bg-white  m-1" href="http://youtube.com/"><i className="fa fa-youtube fa-2x text-danger p-1"></i></a>
+                        <a className="btn btn-social-icon bg-white  m-1" href="mailto:"><i className="fa fa-envelope-o fa-2x text-danger p-1"></i></a>
                     </div>
                 </div>
             </div>
